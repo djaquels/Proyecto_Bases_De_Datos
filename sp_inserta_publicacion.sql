e5949882077ea9923065e58aa5cb78801516e7a4
= <hjacales@gmail.com>
2019-04-18 13:57:15 -0500

    Correciones de Sintaxis

--- sp_inserta_publicacion.sql
+++ sp_inserta_publicacion.sql
@@ -2,6 +2,6 @@ CREATE PROCEDURE sp_inserta_publicacion @nombre varchar(25),@ideditorial char(5)
 AS
 BEGIN
 DECLARE @id char(5)
-SELECT @id = select 'edi' + convert(varchar(2),count(id_publicacion)) FROM Publicaciones
-INSERT INTO Publicaciones (id_publicacion),id_editorial,nombre_publicacion,frecuencia) VALUES (@id,@ideditorial,@nombre,@frecuencia)
+SET @id = (select 'edi' + convert(varchar(2),count(id_publicacion)) FROM Publicaciones)
+INSERT INTO Publicaciones (id_publicacion,id_editorial,nombre_publicacion,frecuencia) VALUES (@id,@ideditorial,@nombre,@frecuencia)
 END
