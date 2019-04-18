eff40775538bf6251d686002c5d931aec1d65b37
= <hjacales@gmail.com>
2019-04-18 14:10:38 -0500

    Correciones de Sintaxis

--- sp_inserta_publicacion.sql
+++ sp_inserta_publicacion.sql
@@ -1,7 +1,7 @@
-CREATE PROCEDURE sp_inserta_publicacion @nombre varchar(25),@ideditorial char(5),frecuencia int
+CREATE PROCEDURE sp_inserta_publicacion @nombre varchar(25),@ideditorial char(5),@frecuencia int
 AS
 BEGIN
 DECLARE @id char(5)
-SET @id = (select 'edi' + convert(varchar(2),count(id_publicacion)) FROM Publicaciones)
+SET @id = (select 'pub' + convert(varchar(2),count(id_publicacion) + 1) FROM Publicaciones)
 INSERT INTO Publicaciones (id_publicacion,id_editorial,nombre_publicacion,frecuencia) VALUES (@id,@ideditorial,@nombre,@frecuencia)
 END
