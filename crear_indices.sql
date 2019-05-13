052512974f520290e8c4f5b26567c388b615d154
= <hjacales@gmail.com>
2019-05-12 20:58:04 -0500

    Bases

---
+++ crear_indices.sql
@@ -0,0 +1,3 @@
+﻿create index nom_publicacion on Publicaciones(nombre_publicacion)
+create index nom_suscriptor on Suscriptores(nombre_susc)
+create index nom_escritor  on Escritores(nombre_escritor)
